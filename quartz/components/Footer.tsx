881912ed63d0ac953d75182ae9d206a534447387
xh3b4sd <xh3b4sd@gmail.com>
2025-04-01 12:35:02 +0200

    Link (#14)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    Co-authored-by: Emile Bangma <github@emilebangma.com>
    Co-authored-by: Stephen Tse <Stephen-X@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,26 +1,27 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
         <ul>
+          <li>
+            <a href="https://quartz.jzhao.xyz/" target="_blank">
+              Quartz v{version}
+            </a>
+          </li>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <a href={link} target="_blank">
+                {text}
+              </a>
             </li>
           ))}
         </ul>
